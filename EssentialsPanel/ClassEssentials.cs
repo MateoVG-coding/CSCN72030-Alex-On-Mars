3ef4a8c0e0340444ef15dc00d96a6ef1fa8f8468
MateoVG-coding <mateovega10@hotmail.com>
2022-11-19 13:40:30 -0500

    Fix EssentialsPanel Class setters and getters.

--- EssentialsPanel/ClassEssentials.cs
+++ EssentialsPanel/ClassEssentials.cs
@@ -8,40 +8,8 @@ namespace EssentialsPanel
     {
         private double oxygen_level;
         private double water_level = 20000;
-        public double desiredWater;
-        private double newWaterLevel;
         private string[] oxygenUseLevel = new string[6];
         private string[] waterUseLevel = new string[6];
-
-        public double oxygenLevel()
-        {
-            Random rand = new Random();
-            double oxyLevel = rand.Next(0, 101); //returns random number between 0-100
-            return oxyLevel;
-        }
-
-        //public double waterLevel()
-        //{
-        //    Random rand = new Random();
-        //    double waterLevel = rand.Next(0, 101); //returns random number between 0-100
-        //    return waterLevel;
-        //}
-
-        /// ///////////////////////////
-        public int CheckLevels()
-        {
-           if (oxygen_level < 25)
-            {
-                Console.WriteLine("Warning!! Oxygen Level is below 25%");
-            }
-           else if (water_level < 25)
-            {
-                Console.WriteLine("Warning!! Water Level is below 25%");
-            }
-
-            return 0;
-        }
-
         public void oxygenUsed(double usedOxygen)
         {
             StreamReader reader;
@@ -69,22 +37,6 @@ namespace EssentialsPanel
                 reader.Close();
             }
         }
-
-        public double WaterUsed(double desiredWater)
-        {
-            if (water_level < desiredWater)
-            {
-                return 0;
-            }
-            else
-            {
-                double newWaterLevel = desiredWater - water_level;
-                return newWaterLevel;
-            }
-        }
-
-
-
         public double getOxygenLevel()
         {
             return oxygen_level;
@@ -92,7 +44,7 @@ namespace EssentialsPanel
 
         public double getWaterLevel()
         {
-            return desiredWater;
+            return water_level;
         }
         
         public void setOxygenLevel(double oxyLevel)
@@ -100,10 +52,11 @@ namespace EssentialsPanel
             this.oxygen_level = oxyLevel;
         }
 
-        public void setWaterLevel()
+        public void setWaterLevel(double desiredWaterLt)
         {
-            double newWater_level = water_level - desiredWater;
-            if (newWaterLevel > 50)
+            double newWaterLevel = getWaterLevel() - desiredWaterLt;
+
+            if(newWaterLevel > 50)
             {
                 this.water_level = newWaterLevel;
             }
