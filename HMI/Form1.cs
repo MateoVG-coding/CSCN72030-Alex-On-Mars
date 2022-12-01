4a6152e85dd9256087f125c89276be9a557e28cb
Jose Ibanez Polo <Jibanezpolo3629@conestogac.on.ca>
2022-12-01 15:51:09 -0500

    Revert "Add completed PlantsPanel to GUI."
    This reverts commit 2979ee84560c35670ab22ea3e6fc73ba79ce4d08.

--- HMI/Form1.cs
+++ HMI/Form1.cs
@@ -19,10 +19,6 @@ namespace HMI
 {
     public partial class Form1 : Form
     {
-        private CancellationTokenSource ctsTemperature;
-
-        private CancellationTokenSource ctsHumidity;
-
         PlantsPanel.PlantsPanel plantsPanel = new PlantsPanel.PlantsPanel();
         Temperature temperature = new Temperature();
         Humidity humidity = new Humidity();
@@ -99,16 +95,8 @@ namespace HMI
         {
 
         }
-
-        private async void roundButton4_Click(object sender, EventArgs e)
+        private void roundButton4_Click(object sender, EventArgs e)
         {
-            if (ctsTemperature != null)
-            {
-                ctsTemperature.Cancel();
-            }
-            // Create a CTS for this request.
-            ctsTemperature = new CancellationTokenSource();
-
             double desiredTemperature = Convert.ToDouble(numericUpDownTemperaturePlants.Text);
 
             plantsPanel.setTemperaturePlants(plantsPanel, desiredTemperature);
@@ -119,87 +107,13 @@ namespace HMI
 
             Cursor.Current = Cursors.Default;
 
-            try
-            {
-                await readFileTemperature(ctsTemperature.Token, 4000);
-            }
-            catch (OperationCanceledException)
-            {
-            }
-        }
-
-        private async Task readFileTemperature(CancellationToken token, int time)
-        {
             string[] lines = File.ReadAllLines("FileTemperature.txt");
-
-            for(int i = 0; i < lines.Length; i++)
-            {
-                await Task.Delay(time, token);
-                label11.Text = lines[i];
-            }
-        }
-
-        private async void roundButtonHumidityPlants_Click(object sender, EventArgs e)
-        {
-            if (ctsHumidity != null)
-            {
-                ctsHumidity.Cancel();
-            }
-            // Create a CTS for this request.
-            ctsHumidity = new CancellationTokenSource();
-
-            double desiredHumdity = Convert.ToDouble(numericUpDownHumidityPlants.Text);
-
-            plantsPanel.setHumidityPlants(plantsPanel, desiredHumdity);
-
-            Cursor.Current = Cursors.WaitCursor;
-
-            plantsPanel.createFileHumidity(plantsPanel);
-
-            Cursor.Current = Cursors.Default;
-
-            try
-            {
-                await readFileHumidity(ctsHumidity.Token, 2000);
-            }
-            catch (OperationCanceledException)
-            {
-            }
-        }
-
-        private async Task readFileHumidity(CancellationToken token, int time)
-        {
-            string[] lines = File.ReadAllLines("FileHumidity.txt");
 
             for (int i = 0; i < lines.Length; i++)
             {
-                await Task.Delay(time, token);
-                label13.Text = lines[i];
+                label11.Text = lines[i];
+                WaitNSeconds(4);
             }
-        }
-
-        private void roundButtonWaterPlants_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void roundButtonLightIntensity_Click(object sender, EventArgs e)
-        {
-            string desiredLightIntensity = comboBoxLightIntensity.Text;
-
-            if (desiredLightIntensity == "High")
-            {
-                plantsPanel.setLightIntensityPlants(plantsPanel, 2);
-            }
-            else if(desiredLightIntensity == "Medium")
-            {
-                plantsPanel.setLightIntensityPlants(plantsPanel, 1);
-            }
-            else
-            {
-                plantsPanel.setLightIntensityPlants(plantsPanel, 0);
-            }
-
-            label14.Text = desiredLightIntensity;
         }
 
         private void WaitNSeconds(int seconds)
@@ -467,21 +381,6 @@ namespace HMI
                 label11.Text = lines[i];
                 WaitNSeconds(4);
             }
-        }
-
-        private void roundButton7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void roundButton8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void roundButton9_Click(object sender, EventArgs e)
-        {
-
         }
     }
 
